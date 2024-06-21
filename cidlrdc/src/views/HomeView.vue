4eb4c0937d759685ca3714b64020a39c496c6076
Dan Malengela <93791798+DanMM7@users.noreply.github.com>
2024-06-20 23:18:58 -0500

    Added page component for home page

--- cidlrdc/src/views/HomeView.vue
+++ cidlrdc/src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import About from '../components/AboutSection.vue'
+import Speaker from '../components/SpeakerSection.vue'
+import Schedule from '../components/ScheduleSection.vue'
+import Venue from '../components/VenueSection.vue'
+import Gallery from '../components/GallerySection.vue'
+import Sponsor from '../components/SponsorSection.vue'
+import FAQ from '../components/FaqSection.vue'
+import Subscribe from '../components/SubscribeSection.vue'
+import Ticket from '../components/TicketSection.vue'
+import Contact from '../components/ContactSection.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main id="main">
+    <About />
+    <Speaker />
+    <Schedule />
+    <Venue />
+    <Gallery />
+    <Sponsor />
+    <FAQ />
+    <Subscribe />
+    <Ticket />
+    <Contact />
   </main>
 </template>
